c2658c39aa041b07729745807b0e1615e8c9f568
Tien <dongoctientb@gmail.com>
2025-06-20 09:28:03 +0700

    reset detail

--- web/page/hotel/detail/detail.php
+++ web/page/hotel/detail/detail.php
@@ -1,4 +1,7 @@
 <?
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 include('data_detail.php');
 ?>
 <!DOCTYPE html>
